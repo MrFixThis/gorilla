03c8c02c7d31cdd64beedb6f75689ab4393cbcf7
MrFixThis <mrfixthis55@gmail.com>
2023-09-05 10:19:00 -0500

    style(lexer): avoid enum glob imports
    Eliminate the glob imports in the lexer implementation to improve code readability
    and soundness.

--- crates/lexer/src/lib.rs
+++ crates/lexer/src/lib.rs
@@ -11,14 +11,14 @@ mod tests;
 pub mod keyword;
 pub mod token;
 
-use token::BinOpToken::{self, *};
-use token::CommentKind::*;
-use token::DelimiterToken::*;
-use token::LitKind::*;
+use token::BinOpToken as BoT;
+use token::CommentKind as Ck;
+use token::DelimiterToken as Dt;
+use token::LitKind as Lk;
 use token::Span;
 use token::Symbol;
 use token::Token;
-use token::TokenKind::{self, *};
+use token::TokenKind as Tk;
 
 pub struct Lexer {
     pos: Cell<usize>,
@@ -58,49 +58,46 @@ impl Lexer {
                 b':' => self.read_colon(),
                 b'-' => self.read_minus(),
                 b'\'' | b'"' => self.read_str(),
-                b'<' => self.read_angle(Shl, Lt, Le),
-                b'>' => self.read_angle(Shr, Gt, Ge),
-                b'|' => self.read_bw_or_bool(Or, OrOr),
-                b'&' => self.read_bw_or_bool(And, AndAnd),
-                b'^' => self.read_bin_op(Caret),
-                b'+' => self.read_bin_op(Plus),
-                b'%' => self.read_bin_op(Percent),
-                b'*' => self.read_comp_bin_op(Star, StarStar),
-                b'/' => self.read_comp_bin_op(Slash, SlashSlash),
-                b'@' => self.build_tok(self.pos(), At),
-                b',' => self.build_tok(self.pos(), Comma),
-                b'~' => self.build_tok(self.pos(), Tilde),
-                b';' => self.build_tok(self.pos(), Semicolon),
-                b'{' => self.build_tok(self.pos(), OpenDel(Brace)),
-                b'}' => self.build_tok(self.pos(), CloseDel(Brace)),
-                b'[' => self.build_tok(self.pos(), OpenDel(Bracket)),
-                b']' => self.build_tok(self.pos(), CloseDel(Bracket)),
-                b'(' => self.build_tok(self.pos(), OpenDel(Parenthesis)),
-                b')' => self.build_tok(self.pos(), CloseDel(Parenthesis)),
+                b'<' => self.read_angle(BoT::Shl, Tk::Lt, Tk::Le),
+                b'>' => self.read_angle(BoT::Shr, Tk::Gt, Tk::Ge),
+                b'|' => self.read_bw_or_bool(BoT::Or, Tk::OrOr),
+                b'&' => self.read_bw_or_bool(BoT::And, Tk::AndAnd),
+                b'^' => self.read_bin_op(BoT::Caret),
+                b'+' => self.read_bin_op(BoT::Plus),
+                b'%' => self.read_bin_op(BoT::Percent),
+                b'*' => self.read_comp_bin_op(BoT::Star, BoT::StarStar),
+                b'/' => self.read_comp_bin_op(BoT::Slash, BoT::SlashSlash),
+                b'@' => self.build_tok(self.pos(), Tk::At),
+                b',' => self.build_tok(self.pos(), Tk::Comma),
+                b'~' => self.build_tok(self.pos(), Tk::Tilde),
+                b';' => self.build_tok(self.pos(), Tk::Semicolon),
+                b'{' => self.build_tok(self.pos(), Tk::OpenDel(Dt::Brace)),
+                b'}' => self.build_tok(self.pos(), Tk::CloseDel(Dt::Brace)),
+                b'[' => self.build_tok(self.pos(), Tk::OpenDel(Dt::Bracket)),
+                b']' => self.build_tok(self.pos(), Tk::CloseDel(Dt::Bracket)),
+                b'(' => self.build_tok(self.pos(), Tk::OpenDel(Dt::Parenthesis)),
+                b')' => self.build_tok(self.pos(), Tk::CloseDel(Dt::Parenthesis)),
                 r if r.is_ascii_digit() => return self.read_number(),
                 r if r.is_ascii_alphabetic() || r == b'_' => return self.read_ident_or_kw(),
                 _ => {
                     let pos = self.pos();
-                    self.build_tok(
-                        pos,
-                        TokenKind::lit(Error, Symbol::new(&self.bytes[pos..=pos])),
-                    )
+                    self.build_tok(pos, Tk::lit(Lk::Error, Symbol::new(&self.bytes[pos..=pos])))
                 }
             },
-            None => return self.build_tok(0, Eof),
+            None => return self.build_tok(0, Tk::Eof),
         };
         self.adv_pos();
         tok
     }
 
-    fn build_tok<'a>(&'a self, start_pos: usize, kind: TokenKind<'a>) -> Token<'a> {
+    fn build_tok<'a>(&'a self, start_pos: usize, kind: Tk<'a>) -> Token<'a> {
         let span = match kind {
-            Eof => Default::default(),
+            Tk::Eof => Default::default(),
             _ => {
                 let mut rows = self.lns.iter().enumerate();
                 let end_pos = match self.pos() {
                     p if p == start_pos => p,
-                    p => p - 1
+                    p => p - 1,
                 };
 
                 let start_row = rows
@@ -163,7 +160,7 @@ impl Lexer {
         }
     }
 
-    fn read_n_and<'a>(&'a self, n: usize, kind: TokenKind<'a>) -> Token<'a> {
+    fn read_n_and<'a>(&'a self, n: usize, kind: Tk<'a>) -> Token<'a> {
         let start = self.pos();
         (0..=n).for_each(|_| self.adv_pos());
         self.build_tok(start, kind)
@@ -188,83 +185,78 @@ impl Lexer {
     #[inline]
     fn read_minus(&self) -> Token<'_> {
         match self.peek_next() {
-            Some(b'>') => self.read_n_and(1, Arrow),
-            Some(b'=') => self.read_n_and(1, BinOpEq(Minus)),
-            _ => self.build_tok(self.pos(), BinOp(Minus)),
+            Some(b'>') => self.read_n_and(1, Tk::Arrow),
+            Some(b'=') => self.read_n_and(1, Tk::BinOpEq(BoT::Minus)),
+            _ => self.build_tok(self.pos(), Tk::BinOp(BoT::Minus)),
         }
     }
 
     #[inline]
     fn read_not(&self) -> Token<'_> {
         match self.peek_next() {
-            Some(b'=') => self.read_n_and(1, Ne),
-            _ => self.build_tok(self.pos(), Not),
+            Some(b'=') => self.read_n_and(1, Tk::Ne),
+            _ => self.build_tok(self.pos(), Tk::Not),
         }
     }
 
     #[inline]
     fn read_dot(&self) -> Token<'_> {
         match (self.peek_next(), self.peek_n(2)) {
-            (Some(b'.'), Some(b'.')) => self.read_n_and(2, Ellipsis),
-            (Some(b'.'), _) => self.read_n_and(1, DotDot),
-            _ => self.build_tok(self.pos(), Dot),
+            (Some(b'.'), Some(b'.')) => self.read_n_and(2, Tk::Ellipsis),
+            (Some(b'.'), _) => self.read_n_and(1, Tk::DotDot),
+            _ => self.build_tok(self.pos(), Tk::Dot),
         }
     }
 
     #[inline]
     fn read_eq(&self) -> Token<'_> {
         match self.peek_next() {
-            Some(b'=') => self.read_n_and(1, EqEq),
-            Some(b'>') => self.read_n_and(1, FatArrow),
-            _ => self.build_tok(self.pos(), Eq),
+            Some(b'=') => self.read_n_and(1, Tk::EqEq),
+            Some(b'>') => self.read_n_and(1, Tk::FatArrow),
+            _ => self.build_tok(self.pos(), Tk::Eq),
         }
     }
 
     #[inline]
     fn read_colon(&self) -> Token<'_> {
         match self.peek_next() {
-            Some(b':') => self.read_n_and(1, ColonColon),
-            _ => self.build_tok(self.pos(), Colon),
+            Some(b':') => self.read_n_and(1, Tk::ColonColon),
+            _ => self.build_tok(self.pos(), Tk::Colon),
         }
     }
 
-    fn read_bw_or_bool<'a>(&'a self, bw: BinOpToken, bool: TokenKind<'a>) -> Token<'a> {
+    fn read_bw_or_bool<'a>(&'a self, bw: BoT, bool: Tk<'a>) -> Token<'a> {
         match self.peek_next() {
             Some(b) if b == self.byte().unwrap() => self.read_n_and(1, bool),
-            Some(b'=') => self.read_n_and(1, BinOpEq(bw)),
-            _ => self.build_tok(self.pos(), BinOp(bw)),
+            Some(b'=') => self.read_n_and(1, Tk::BinOpEq(bw)),
+            _ => self.build_tok(self.pos(), Tk::BinOp(bw)),
         }
     }
 
-    fn read_bin_op(&self, bin_op: BinOpToken) -> Token<'_> {
+    fn read_bin_op(&self, bin_op: BoT) -> Token<'_> {
         match self.peek_next() {
-            Some(b'=') => self.read_n_and(1, BinOpEq(bin_op)),
-            _ => self.build_tok(self.pos(), BinOp(bin_op)),
+            Some(b'=') => self.read_n_and(1, Tk::BinOpEq(bin_op)),
+            _ => self.build_tok(self.pos(), Tk::BinOp(bin_op)),
         }
     }
 
-    fn read_comp_bin_op(&self, sing_op: BinOpToken, comp_op: BinOpToken) -> Token<'_> {
+    fn read_comp_bin_op(&self, sing_op: BoT, comp_op: BoT) -> Token<'_> {
         match self.peek_next() {
             Some(n) if n == self.byte().unwrap() => match self.peek_n(2) {
-                Some(b'=') => self.read_n_and(2, BinOpEq(comp_op)),
-                _ => self.read_n_and(1, BinOp(comp_op)),
+                Some(b'=') => self.read_n_and(2, Tk::BinOpEq(comp_op)),
+                _ => self.read_n_and(1, Tk::BinOp(comp_op)),
             },
-            Some(b'=') => self.read_n_and(1, BinOpEq(sing_op)),
-            _ => self.build_tok(self.pos(), BinOp(sing_op)),
+            Some(b'=') => self.read_n_and(1, Tk::BinOpEq(sing_op)),
+            _ => self.build_tok(self.pos(), Tk::BinOp(sing_op)),
         }
     }
 
-    // HACK: Eliminate boilerplate ^^
-    fn read_angle<'a>(
-        &'a self,
-        bw: BinOpToken,
-        bool: TokenKind<'a>,
-        bool_eq: TokenKind<'a>,
-    ) -> Token<'a> {
+    // HACk: Eliminate boilerplate ^^
+    fn read_angle<'a>(&'a self, bw: BoT, bool: Tk<'a>, bool_eq: Tk<'a>) -> Token<'a> {
         match self.peek_next() {
             Some(n) if n == self.byte().unwrap() => match self.peek_n(2) {
-                Some(b'=') => self.read_n_and(2, BinOpEq(bw)),
-                _ => self.build_tok(self.pos(), BinOp(bw)),
+                Some(b'=') => self.read_n_and(2, Tk::BinOpEq(bw)),
+                _ => self.build_tok(self.pos(), Tk::BinOp(bw)),
             },
             Some(b'=') => self.read_n_and(1, bool_eq),
             _ => self.build_tok(self.pos(), bool),
@@ -284,12 +276,12 @@ impl Lexer {
                     Some(b'#') => {
                         self.adv_pos();
                         self.adv_pos();
-                        self.build_tok(org_pos, Comment(Block, Symbol::new(&self.bytes[s..l])))
+                        self.build_tok(org_pos, Tk::Comment(Ck::Block, Symbol::new(&self.bytes[s..l])))
                     },
                     _ => {
                         self.build_tok(
                             org_pos,
-                            TokenKind::lit(Error, Symbol::new(&self.bytes[org_pos..=l - 1]))
+                            Tk::lit(Lk::Error, Symbol::new(&self.bytes[org_pos..=l - 1]))
                         )
                     },
                 }
@@ -297,7 +289,7 @@ impl Lexer {
         } else {
             self.read_while_and(
                 |b| b != b'\n',
-                |s, l| self.build_tok(org_pos, Comment(Inline, Symbol::new(&self.bytes[s..l])))
+                |s, l| self.build_tok(org_pos, Tk::Comment(Ck::Inline, Symbol::new(&self.bytes[s..l])))
             )
         }
     }
@@ -309,9 +301,9 @@ impl Lexer {
             |s, l| {
                 let sym = Symbol::new(&self.bytes[s..l]);
                 let kind = if keyword::is_keyword(&sym) {
-                    Keyword(sym)
+                    Tk::Keyword(sym)
                 } else {
-                    Ident(sym)
+                    Tk::Ident(sym)
                 };
                 self.build_tok(s, kind)
             },
@@ -328,10 +320,10 @@ impl Lexer {
             |b| b != b'"' && b != b'\'',
             |s, l| {
                 if let Some(&c) = self.byte() && c == f {
-                    self.build_tok(org_pos, TokenKind::lit(Str, Symbol::new(&self.bytes[s..l])))
+                    self.build_tok(org_pos, Tk::lit(Lk::Str, Symbol::new(&self.bytes[s..l])))
                 } else {
                     self.build_tok(
-                        org_pos, TokenKind::lit(Error, Symbol::new(&self.bytes[org_pos..=l]))
+                        org_pos, Tk::lit(Lk::Error, Symbol::new(&self.bytes[org_pos..=l]))
                     )
                 }
             },
@@ -346,12 +338,12 @@ impl Lexer {
                 let (kind, bytes) = {
                     let bytes = &self.bytes[s..l];
                     if bytes.contains(&b'.') {
-                        (Float, bytes)
+                        (Lk::Float, bytes)
                     } else {
-                        (Int, bytes)
+                        (Lk::Int, bytes)
                     }
                 };
-                self.build_tok(s, TokenKind::lit(kind, Symbol::new(bytes)))
+                self.build_tok(s, Tk::lit(kind, Symbol::new(bytes)))
             },
         )
     } // TODO: look for multiple periods in the float numbers
